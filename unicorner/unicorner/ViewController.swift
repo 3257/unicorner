c9ea731d9c5183ee1154602bcf3e69df9afb9a49
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2018-01-19 18:54:29 +0200

    PNs set-up

---
+++ unicorner/unicorner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  unicorner
+//
+//  Created by Deyan Aleksandrov on 1/19/18.
+//  Copyright © 2018 centroida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var unicornImageView: UIImageView!
+    @IBOutlet weak var unicornName: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
